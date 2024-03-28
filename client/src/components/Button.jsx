db3baa788ff493a19bf3a36a6580f78f9879d1d6
Shivam <shivamjgec@gmail.com>
2024-03-29 01:25:48 +0530

    home

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      <div className="btn">
+        <button>Explore</button>
+        <button>Contact US</button>
+      </div>
+      
+     
+      
+    </div>
+  )
+}
+
+export default Button
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="home">
+        <h1>Unlocking Education:</h1>
+        <h2>Your Free Learning Journey</h2>
+        <p>"Empower Your Mind: Free Education Hub - Dive Into Limitless Learning Opportunities </p> <p>Expert Guidance, and Enriching Resources Now!".</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
