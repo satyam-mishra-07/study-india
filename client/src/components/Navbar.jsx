46a8ce478384a332fe9e4ebbb8fe5f8e7839f0e1
beprodeep4718 <144547279+beprodeep4718@users.noreply.github.com>
2024-03-29 02:17:13 +0530

    done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className='flex w-full px-11 py-4 justify-between items-center'>
+    <nav className='fixed backdrop-blur-[2px] flex w-full px-11 py-4 justify-between items-center'>
       <div className="logo">
         <h2 className='font-bold text-4xl text-[var(--primary-color)]'>Study India</h2>
       </div>
       <div className="navItems">
-        <ul className='flex items-center gap-6 text-2xl'>
+        <ul className='flex items-center gap-6 text-2xl font-semibold'>
             <li>Home</li>
             <li>Practice</li>
             <li>Contact</li>
