038eb5d3b4ea27173d9eda6bcaba56d06666dbaf
beprodeep4718 <144547279+beprodeep4718@users.noreply.github.com>
2024-03-29 01:27:10 +0530

    done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className='flex'>
+    <nav className='flex w-full px-11 py-4 justify-between items-center'>
       <div className="logo">
-        <h2>Study India</h2>
+        <h2 className='font-bold text-4xl text-[var(--primary-color)]'>Study India</h2>
       </div>
       <div className="navItems">
-        <ul>
+        <ul className='flex items-center gap-6 text-2xl'>
             <li>Home</li>
             <li>Practice</li>
             <li>Contact</li>
-            <li>About</li>
+            <li>About</li> 
         </ul>
       </div>
     </nav>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["*.{html,js.jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
